b86b6b2af332401f12f45ccb1bedbdb428cba417
Sergey V. Levchenko <s77777.list.ru>
2018-09-10 08:03:07 +0300

    refs #test

--- public/assets/js/run.js
+++ public/assets/js/run.js
@@ -2,5 +2,11 @@
     d.addEventListener('DOMContentLoaded',function(){
         d.querySelector('button').addEventListener('click',Action);
     });
+    d.forms[0].querySelector('input[type="file"]').addEventListener('change',function(){
+        var form = d.forms[0];
+        var file = form.file.files[0];
+        form.querySelector('.custom-file-label').textContent=file.name;
+        console.log(file.name);
+    });
 })();
 
